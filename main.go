65bcd79ef53d6528e1a744c4267d38310c652332
Frank Moley <frankmoley@gmail.com>
2023-06-08 02:47:15 -0500

    adding encapsulation to dog adding NewDog method modifying main to leverage encapsulation

--- main.go
+++ main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/fpmoles/treating-go-as-an-object-oriented-language-4401616/pets"
 )
 
 func main() {
-	pet := pets.Dog{
-		Name:  "Oreo",
-		Color: "Black and White",
-		Breed: "Mixed",
-	}
+	//sleepTime := time.Now()
+	sleepTime := time.Now().Add(time.Duration(-5) * time.Hour)
+	pet := pets.NewDog("Oreo", "Black and White", "Mixed", sleepTime)
 	fmt.Println(pet.Feed("steak"))
 	fmt.Println(pet.GiveAttention("play fetch"))
 }
--- pets/dog.go
+++ pets/dog.go
@@ -3,12 +3,22 @@ package pets
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 type Dog struct {
-	Name  string
-	Color string
-	Breed string
+	Name      string
+	Color     string
+	Breed     string
+	lastSlept time.Time
+}
+
+func (d Dog) needsSleep() bool {
+	return time.Now().Sub(d.lastSlept) > 4*time.Hour
+}
+
+func (d Dog) sleep() {
+	d.lastSlept = time.Now()
 }
 
 func (d Dog) Feed(food string) string {
@@ -16,6 +26,10 @@ func (d Dog) Feed(food string) string {
 }
 
 func (d Dog) GiveAttention(activity string) string {
+	if d.needsSleep() {
+		d.sleep()
+		return "Your dog is tired and needs to rest"
+	}
 	response := ""
 	switch strings.ToUpper(activity) {
 	case "PET":
@@ -26,4 +40,13 @@ func (d Dog) GiveAttention(activity string) string {
 		response = "barks"
 	}
 	return fmt.Sprintf("%s loves attention, %s will cause him to %s", d.Name, activity, response)
+}
+
+func NewDog(name, color, breed string, lastSlept time.Time) Dog {
+	return Dog{
+		Name:      name,
+		Color:     color,
+		Breed:     breed,
+		lastSlept: lastSlept,
+	}
 }
